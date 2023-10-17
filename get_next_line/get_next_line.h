479a30140b81908d9c4db7df350b9375e7ed045d
Kayhan Moshkeri <kmoshker@c1r23s11.42tokyo.jp>
2023-10-17 21:01:30 +0900

    from school pc

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,20 @@
 /*   By: kmoshker <kmoshker@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 21:24:22 by kmoshker          #+#    #+#             */
-/*   Updated: 2023/10/16 21:25:33 by kmoshker         ###   ########.fr       */
+/*   Updated: 2023/10/17 16:42:58 by kmoshker         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <>
 
 char	*get_next_line(int fd);
 
